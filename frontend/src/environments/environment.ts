c73e4136800081267296e451edac5107125ec2d9
Kavya <jainkavya6890@gmail.com>
2022-09-17 16:47:14 +0545

    Restructured auth config

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-40iwt9kb.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-40iwt9kb.us', // the auth0 domain prefix
     audience: 'https://coffee-shop', // the audience set for the auth0 app
     clientId: 'uFdBtJhHrFfhMHQtlFU0YmykU9zEZZ0v', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:4200', // the base url of the running ionic application. 
